c9dbb5105a640ed71c6453f1eef751333c189dc0
Edin Dzanic <edin.dz@hotmail.com>
2018-10-12 10:18:07 +0200

    Add basic structure with placeholder data

--- src/App.js
+++ src/App.js
@@ -5,21 +5,42 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App p-2">
+        <form action="">
+          <div className="form-group">
+            <div className="btn-group" role="group">
+              <button type="button" className="btn btn-primary">Movies</button>
+              <button type="button" className="btn btn-outline-primary">TV Shows</button>
+            </div>
+          </div>
+          <div className="form-group">
+            <label className="sr-only" htmlFor="search">Search</label>
+            <input type="search" className="form-control" id="search" placeholder="Search" />
+          </div>
+        </form>
+
+        <div className="d-flex p-2 justify-content-between flex-wrap" id="movies">
+          <div className="card">
+            <img className="card-img-top" src="https://via.placeholder.com/350x200" alt="Movie" />
+            <div className="card-body">
+              <h2 className="card-title text-center">Movie Title</h2>
+            </div>
+          </div>
+
+          <div className="card">
+            <img className="card-img-top" src="https://via.placeholder.com/350x200" alt="Movie" />
+            <div className="card-body">
+              <h2 className="card-title text-center">Movie Title</h2>
+            </div>
+          </div>
+
+          <div className="card">
+            <img className="card-img-top" src="https://via.placeholder.com/350x200" alt="Movie" />
+            <div className="card-body">
+              <h2 className="card-title text-center">Movie Title</h2>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
