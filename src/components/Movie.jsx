7cca8d7672c5763865251c3c204bff086901521c
Edin Dzanic <edin.dz@hotmail.com>
2018-10-14 17:10:15 +0200

    Change Movies and Movie components

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
+import posterPlaceholder from './../images/placeholder.jpg';
 
-export default function Movie() {
-    return (
-        <div className="card">
-            <img className="card-img-top" src="https://via.placeholder.com/350x200" alt="Movie" />
-            <div className="card-body">
-              <h2 className="card-title text-center">Movie Title</h2>
+const POSTER_URL = "https://image.tmdb.org/t/p/w400"
+
+class Movie extends Component {
+
+    render() {
+        const { poster_path, name, onClick, id } = this.props;
+        const src = poster_path ? POSTER_URL + poster_path : posterPlaceholder;
+        return (
+            <div className="col-6">
+                <div className="card m-5" onClick={() => onClick(id)}>
+                    <img className="card-img-top" src={src} alt="Movie" />
+                    <div className="card-body">
+                        <h2 className="card-title text-center">{name}</h2>
+                    </div>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
+export default Movie;
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -2,13 +2,18 @@ import React from "react";
 
 import Movie from './Movie';
 
-export default function Movies() {
+export default function Movies(props) {
+    const {items, onClick} = props;
     return (
-        <div className="d-flex p-2 justify-content-between flex-wrap">
-          <Movie />
-          <Movie />
-          <Movie />
-          <Movie />
+        <div className="d-flex flex-row p-2 justify-content-center flex-wrap">
+            {items.map((item, i) => 
+                <Movie 
+                    key={i} 
+                    name={item.name || item.title} 
+                    poster_path={item.poster_path} 
+                    onClick={onClick}
+                    id={item.id}
+                />)}
         </div>
     );
 };
