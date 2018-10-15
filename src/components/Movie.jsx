7e55909bcb86210177c1d60b05d39d3b9f6cdd64
Edin Dzanic <edin.dz@hotmail.com>
2018-10-15 13:41:32 +0200

    Change Movie component to use Poster component

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,19 +1,16 @@
 import React, { Component } from "react";
-import posterPlaceholder from './../images/placeholder.jpg';
-
-const POSTER_URL = "https://image.tmdb.org/t/p/w400"
+import Poster from './Poster';
 
 class Movie extends Component {
 
     render() {
         const { poster_path, name, onClick, id } = this.props;
-        const src = poster_path ? POSTER_URL + poster_path : posterPlaceholder;
         return (
-            <div className="col-6">
-                <div className="card m-5" onClick={() => onClick(id)}>
-                    <img className="card-img-top" src={src} alt="Movie" />
-                    <div className="card-body">
-                        <h2 className="card-title text-center">{name}</h2>
+            <div className="col-6 mb-4">
+                <div className="card h-100" onClick={() => onClick(id)}>
+                    <Poster className="card-img-top" posterPath={poster_path} alt="Movie"/>
+                    <div className="d-flex card-body justify-content-center align-items-center">
+                        <p className="card-title text-center">{name}</p>
                     </div>
                 </div>
             </div>
