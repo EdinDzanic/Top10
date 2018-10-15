4d049a3ef3ce674aa084eebf83058965f4704e40
Edin Dzanic <edin.dz@hotmail.com>
2018-10-15 14:53:23 +0200

    Add notice if no results were found to ListView

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -3,17 +3,20 @@ import React from "react";
 import Movie from './Movie';
 
 export default function Movies(props) {
-    const {items, onClick} = props;
+    const { items, onClick } = props;
     return (
         <div className="d-flex flex-row p-2 justify-content-center flex-wrap">
-            {items.map((item, i) => 
-                <Movie 
-                    key={i} 
-                    name={item.name || item.title} 
-                    poster_path={item.poster_path} 
-                    onClick={onClick}
-                    id={item.id}
-                />)}
+            {items.length > 0 ?
+                items.map((item, i) =>
+                    <Movie
+                        key={i}
+                        name={item.name || item.title}
+                        poster_path={item.poster_path}
+                        onClick={onClick}
+                        id={item.id}
+                    />)
+                : <p className="notice text-muted">No results found.</p>
+            }
         </div>
     );
 };
