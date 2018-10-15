e5322d9f76f276f2d0119d411d3a1f6e50b9a24b
Edin Dzanic <edin.dz@hotmail.com>
2018-10-15 16:06:17 +0200

    Change preview to get video of type trailer instead of first video

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -3,11 +3,16 @@ import Video from './Video';
 import Poster from './Poster';
 
 export default function Preview({ videos, posterPath}) {
-    const hasTrailer = videos !== undefined && videos.results && videos.results.length > 0;
+    let hasTrailer = videos !== undefined && videos.results && videos.results.length > 0;
+    let trailer;
+    if (hasTrailer)
+        trailer = videos.results.find(x => x.type === 'Trailer');
+    
+    hasTrailer = trailer !== undefined;
     return (
         <React.Fragment>
             {hasTrailer ?
-                <Video video={videos.results[0]} /> :
+                <Video video={trailer} /> :
                 <Poster className="mb-2" posterPath={posterPath} alt="Movie" />
             }
         </React.Fragment>
