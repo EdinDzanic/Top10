9876ccec8a7429fa0a75af50a7a79773b01e5ad6
Edin Dzanic <edin.dz@hotmail.com>
2018-10-16 10:46:47 +0200

    Change placeholder and size of posters

--- src/components/Poster.jsx
+++ src/components/Poster.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import posterPlaceholder from './../images/placeholder.jpg';
+import posterPlaceholder from './../images/placeholder-image.jpg';
 
-const POSTER_URL = "https://image.tmdb.org/t/p/w400"
+const POSTER_URL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2/"
 
 export default function Poster({posterPath, alt, className=""}) {
     const src = posterPath ? POSTER_URL + posterPath : posterPlaceholder;
